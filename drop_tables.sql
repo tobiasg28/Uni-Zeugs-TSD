9f7ab20402113e17b3d4a48ab79cc4a7ae5bf53b
Martin Wieser <wi3s3r@Martin-Wiesers-MacBook-Pro.local>
2011-06-10 17:53:35 +0200

    again drop_table fix

---
+++ drop_tables.sql
@@ -0,0 +1,18 @@
+drop sequence hibernate_sequence cascade;
+drop table swag_user cascade;
+drop table swag_message cascade;
+drop table swag_gamemap cascade;
+drop table swag_resource cascade;
+drop table swag_participation cascade;
+drop table swag_gamestep cascade;
+drop table swag_troop cascade;
+drop table swag_trooptype cascade;
+drop table swag_square cascade;
+drop table swag_itemtype cascade;
+drop table swag_cachedinvalidationentry cascade;
+drop table swag_buildingtype cascade;
+drop table swag_building cascade;
+drop table swag_base cascade;
+drop table swag_resourceamount cascade;
+drop table swag_participation_swag_resourceamount;
+
